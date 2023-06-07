0eaaa2d4e359991afd69dba5d861b6bf84eed093
Mohammad Zarrabi <mhzsam@gmail.com>
2023-06-07 21:07:29 +0330

    test sucssed

---
+++ RabbitMQ/Publisher/Controllers/RabbitController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using SharedClass;
+using System.Text;
+
+namespace Publisher.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class RabbitController : Controller
+    {
+        private readonly IRabbitMqService _rabbitMqService;
+
+        public RabbitController(IRabbitMqService rabbitMqService)
+        {
+            _rabbitMqService = rabbitMqService;
+        }
+        [HttpPost]
+        public IActionResult SendMessage()
+        {
+            using var connection = _rabbitMqService.CreateChannel();
+            using var model = connection.CreateModel();
+            model.QueueDeclare("test", true, false, false);
+            var body = Encoding.UTF8.GetBytes("Hi mohammad zarrabi ");
+
+            model.BasicPublish("", "test",body:body);
+
+            return Ok();
+        }
+    }
+}
---
+++ RabbitMQ/Publisher/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SharedClass;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<RabbitMqConfiguration>(builder.Configuration.GetSection("RabbitMqConfiguration"));
+builder.Services.AddSingleton<IRabbitMqService, RabbitMqService>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
